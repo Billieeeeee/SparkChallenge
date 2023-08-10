5e5ba6e2686a2773d7294569316a9bf67e564430
Billieeeeee <elliot.agustin.martinez.macias@uabc.edu.mx>
2023-08-09 19:10:28 -0700

    CRUD con entity framework

---
+++ Payments/WebApplication1/Controllers/tblClientesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1;
+
+namespace WebApplication1.Controllers
+{
+    public class tblClientesController : Controller
+    {
+        private PaymentsEntities1 db = new PaymentsEntities1();
+
+        // GET: tblClientes
+        public ActionResult Index()
+        {
+            return View(db.tblCliente.ToList());
+        }
+
+        // GET: tblClientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblCliente tblCliente = db.tblCliente.Find(id);
+            if (tblCliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCliente);
+        }
+
+        // GET: tblClientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tblClientes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idCliente,nombre,apellidos,saldo")] tblCliente tblCliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblCliente.Add(tblCliente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tblCliente);
+        }
+
+        // GET: tblClientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblCliente tblCliente = db.tblCliente.Find(id);
+            if (tblCliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCliente);
+        }
+
+        // POST: tblClientes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idCliente,nombre,apellidos,saldo")] tblCliente tblCliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblCliente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tblCliente);
+        }
+
+        // GET: tblClientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblCliente tblCliente = db.tblCliente.Find(id);
+            if (tblCliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblCliente);
+        }
+
+        // POST: tblClientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblCliente tblCliente = db.tblCliente.Find(id);
+            db.tblCliente.Remove(tblCliente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
